1fd9e3f7b606a533d9b8ac43b4fa9e79fa7ebc5a
Diego Lins de Freitas <diego.lins.freitas@gmail.com>
2020-06-18 17:16:16 -0400

    Backend
    Refactoring beans for auth

--- app/src/main/java/io/clinix/app/controller/AuthController.java
+++ app/src/main/java/io/clinix/app/controller/AuthController.java
@@ -1,19 +1,14 @@
 package io.clinix.app.controller;
 
-import io.clinix.app.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
 import java.util.Collections;
 
 @RestController
@@ -25,11 +20,11 @@ public class AuthController {
     @Autowired
     private PasswordEncoder enc;
     /**
-     * Since i am using the basic auth(out of the box) i just need to send
-     * the username and password in every request since it does not generate a token
+     * i am using the basic auth(out of the box) i just need to send
+     * the username and password in every request, since it does not generate a token
      * or save anything on the session.
      *
-     * this endpoint is used in the login form just to implement the flow.
+     * this endpoint is used in the login form just to implement the login flow.
      * @return
      */
     @GetMapping("/verify-credentials")
@@ -38,8 +33,8 @@ public class AuthController {
     }
 
     @PostMapping(path="/register-account", consumes = "application/json", produces = "application/json")
-    String registerAccount(@RequestBody User user) {
-        UserDetails details = new org.springframework.security.core.userdetails.User(user.getUsername(), enc.encode(user.getPassword()), true, true, true, true, Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+    String registerAccount(@RequestBody Credentials credentials) {
+        UserDetails details = new org.springframework.security.core.userdetails.User(credentials.getUsername(), enc.encode(credentials.getPassword()), true, true, true, true, Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
         users.createUser(details);
         return "{\"status\":\"OK\"}";
     }
