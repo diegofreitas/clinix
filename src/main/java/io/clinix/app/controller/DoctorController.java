76e91909401f116588538f25018aac17d75ca5dd
Diego Lins de Freitas <diego.lins.freitas@gmail.com>
2020-06-20 14:40:43 -0400

    [Backend] Doctors
    Crud implementation

--- src/main/java/io/clinix/app/controller/DoctorController.java
+++ src/main/java/io/clinix/app/controller/DoctorController.java
@@ -1,9 +1,13 @@
 package io.clinix.app.controller;
 
 import io.clinix.app.model.Doctor;
+import io.clinix.app.model.Patient;
 import io.clinix.app.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +21,9 @@ public class DoctorController {
         return repository.findAll();
     }
 
-
+    @PostMapping("/doctors")
+    ResponseEntity<Void> createPatient(@RequestBody Doctor doctor) {
+        repository.save(doctor);
+        return ResponseEntity.ok().build();
+    }
 }
